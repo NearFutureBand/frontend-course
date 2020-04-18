6380923e124987901e7c6ecdf6d6a2bdb3069665
Pavel Belyakov <theflashofsonic@gmail.com>
2020-04-18 22:44:32 +0300

    version on webpack

---
+++ website_4_webpack/README.md
@@ -0,0 +1,3 @@
+# Test website with webpack
+
+https://dev.to/pixelgoo/how-to-configure-webpack-from-scratch-for-a-basic-website-46a5
---
+++ website_4_webpack/src/js/components/Card.js
@@ -0,0 +1,39 @@
+class Card {
+  constructor(data, parentElementId, key) {
+    this.data = data;
+    this.parentElementId = parentElementId;
+    this.key = key;
+    this.myId = `${this.parentElementId}-card-${this.key}`;
+    this.parentElement = window.document.getElementById(parentElementId);
+  }
+
+  render = () => {
+    const wrapperElement = window.document.createElement('div');
+    wrapperElement.className = "card";
+    wrapperElement.id = this.myId;
+    const { name: { first, last }, email, about } = this.data;
+    wrapperElement.innerHTML = `
+      <h2>${first} ${last}</h2>
+      <h3>${email}</h3>
+      <span>${about}</span>
+    `;
+    this.parentElement.appendChild(wrapperElement);
+  }
+
+  getElement = () => {
+    return window.document.getElementById(this.myId);
+  }
+
+  static convertToHtml = (oneCardData) => {
+    const { name, email, about } = oneCardData;
+    return `
+      <div class="card">
+        <h2>${name.first} ${name.last}</h2>
+        <h3>${email}</h3>
+        <span>${about}</span>
+      </div>
+    `;
+  }
+}
+
+export default Card;
