bbb09ce89a396931fd37329e0136101dd2470092
Pavel Belyakov <theflashofsonic@gmail.com>
2020-05-11 20:19:14 +0300

    routing 2 project

---
+++ routing2/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainPage from './pages/Main';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
