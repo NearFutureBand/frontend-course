ed33c972decae403c06720ba2eebb08613c92a30
Pavel Belyakov <theflashofsonic@gmail.com>
2020-05-11 22:59:15 +0300

    routing2 project development 1

---
+++ routing2/src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({ picture, name }) => {
+  return (
+    <div className="card">
+      <img src={picture} className="card-image" />
+      <div className="card-content">
+        <h2>{name.first}</h2>
+        <h4>{name.last}</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
