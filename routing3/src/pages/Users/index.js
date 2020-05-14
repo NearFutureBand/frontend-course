3eee1508fed883250794bb3cdc03ffd1bd655064
Pavel Belyakov <theflashofsonic@gmail.com>
2020-05-15 00:54:43 +0300

    homework2 initial

---
+++ routing3/src/pages/Users/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const UsersPage = () => {
+  return (
+    <div className="page page-users">
+
+
+    </div>
+  );
+}
+
+export default UsersPage;
