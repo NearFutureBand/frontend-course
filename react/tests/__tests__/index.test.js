683509c602a546946c0af48810d1e360e386f085
Pavel Belyakov <theflashofsonic@gmail.com>
2022-02-17 22:18:30 +0300

    tests mock fix

--- react/tests/__tests__/index.test.js
+++ react/tests/__tests__/index.test.js
@@ -6,6 +6,52 @@ const {
   login
 } = require('../index');
 
+const user = {
+   "_id": "5e91ac983ed21bfcc",
+    "index": 2,
+    "guid": "b4820655-1036-46e7-994c-929291a2ff6b",
+    "isActive": true,
+    "balance": "$2,312.43",
+    "picture": "https://avatars.dicebear.com/api/human/c.svg",
+    "age": 39,
+    "eyeColor": "green",
+    "name": {
+      "first": "Barr",
+      "last": "Copeland"
+    },
+    "company": "NIPAZ",
+    "email": "barr.copeland@nipaz.me",
+    "phone": "+18465683597",
+    "address": "793 Anna Court, Wanamie, Virginia, 1398",
+    "about": "Est quis nulla nostrud et. Laboris nulla non adipisicing occaecat dolore. Aute consequat cillum mollit ea cupidatat amet magna magna adipisicing est sint eiusmod fugiat non. Commodo ad fugiat culpa mollit Lorem ea Lorem magna id laborum deserunt adipisicing et. Ut culpa dolore est est nostrud magna id nisi ad elit ullamco. Nisi et sunt sint magna aute aliqua adipisicing fugiat. Quis elit veniam ad id id.",
+    "registered": "Wednesday, November 20, 2019 11:45 PM",
+    "latitude": "69.268872",
+    "longitude": "152.537354",
+    "tags": [
+      "culpa",
+      "est",
+      "sunt",
+      "sint",
+      "proident"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [],
+    "greeting": "Hello, Barr! You have 10 unread messages.",
+    "favoriteFruit": "strawberry",
+    "passwordHash": "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
+};
+
 describe('Sum function', () => {
   test('Should sum two variables', () => {
     expect(sum(5,6)).toBe(11);
@@ -21,7 +67,6 @@ describe('Copy array function', () => {
   const arr = [1,2,3];
   test('Should have the same values', () => {
     expect(copyArray(arr)).toEqual(arr);
-    
   });
 
   test('Should create brand new array', () => {
@@ -51,26 +96,34 @@ describe('Mutable function', () => {
   });
 });
 
-/*describe('Login function testing with mock', () => {
-  jest.mock('../__mocks__/axios');
+describe('Login function testing with mock', () => {
+
+  const axios = jest.createMockFromModule('axios');
+  axios.post = function(url, data) {
+    return new Promise((resolve) => {
+      process.nextTick(() => {
+        resolve({ data: user });
+      });
+    });
+  }
+
   const testUserPhoneNumber = '+18465683597';
   const testUserPassword = '123';
   const userUniqueId = '5e91ac983ed21bfcc3e35c68';
 
-  it ('Should return user data', () => {
+  it('Should return user data', () => {
     login(testUserPhoneNumber, testUserPassword).then((response) => {
-      expect(response._id).toBeDefined();
+      expect(response.data._id).toBeDefined();
+      expect(response.data._id).toBe(userUniqueId);
     });
-  })
+  });
 
-  it ('Work wih resolves', () => {
-    expect(login(testUserPhoneNumber, testUserPassword)).resolves.toHaveProperty('_id', userUniqueId)
-  })
-  
-});*/
+  it('Work wih resolves', () => {
+    expect(login(testUserPhoneNumber, testUserPassword)).resolves.toHaveProperty('data');
+  });
+});
 
 describe('Async function: login', () => {
-
   let loginResponse;
   const testUserPhoneNumber = '+18465683597';
   const testUserPassword = '123';
